97e686b97bdbf08c79772383090a98812b4554ab
Az <103241502+HereLiesAz@users.noreply.github.com>
2026-09-17 12:02:25 -0500

    Fix web scroll state import

--- src/wasmJsMain/kotlin/com/hereliesaz/morphont/App.kt
+++ src/wasmJsMain/kotlin/com/hereliesaz/morphont/App.kt
@@ -1,6 +1,7 @@
 package com.hereliesaz.morphont
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
@@ -12,7 +13,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.rememberScrollState
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.DropdownMenu
