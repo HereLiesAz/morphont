28850d62933073c937122999f75b958664935621
Az <103241502+HereLiesAz@users.noreply.github.com>
2026-09-18 21:15:54 -0500

    Bump Android release to 0.4.3

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.hereliesaz.morphont"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.4.2"
+        versionCode = 6
+        versionName = "0.4.3"
     }
 
     signingConfigs {
